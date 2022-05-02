be813e13ae95142826239bbebdf1dd5e9eda4168
muccidavide <davidemucci0804@gmail.com>
2022-05-02 15:43:22 +0200

    Complete with Bonus

---
+++ vue-cli-first/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Main-Title />
+    <MyJumbo/>
+ 
+  </div>
+</template>
+
+<script>
+import MainTitle from './components/Main-Title.vue'
+import MyJumbo from './components/My-Jumbo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainTitle,
+    MyJumbo
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli-first/src/components/Main-Title.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <h1 class="display-3 mb-3">La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
